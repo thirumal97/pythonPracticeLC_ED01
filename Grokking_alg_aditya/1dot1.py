e14deead7b8613a4841708b4274ec963078cbbe9
thirumal97 <thirumalkonatham1@gmail.com>
2024-06-21 12:46:01 -0700

    adding grokking exercises 1

---
+++ Grokking_alg_aditya/1dot1.py
@@ -0,0 +1,8 @@
+# Suppose you have a sorted list of 128 names, and you’re searching
+# through it using binary search. What’s the maximum number of
+# steps it would take?
+
+# time complexity it would take O(logn)
+
+# log 128 === 7 steps
+
